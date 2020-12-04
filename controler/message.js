0d7ab1582fbc8b8371e695888afee1809550d9fd
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2020-12-04 14:40:01 +0200

    message fields are required

--- controler/message.js
+++ controler/message.js
@@ -35,9 +35,9 @@ const getOnemessage =((req,res,next)=>{
 const createNewmessage= ((req,res,next)=>{
     const message =new Message({
        _id: new mongoose.Types.ObjectId(),
-        title: req.body.title,
-        body: req.body.body,
-        image:req.body.body
+        name: req.body.name,
+        email: req.body.email,
+        message:req.body.message
     });
     message.save().then(result =>{
         console.log(result);
