2e5e11dd4abb5238397c5b12bae639a6bfaeb269
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2021-01-31 20:47:47 +0200

    removing cookies

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,21 +3,11 @@ require("dotenv").config();
 
 module.exports=((req,res,next)=>{
     try{
-        const decoded=null;
-        const token=req.cookies.make;
-        const token2= req.headers.authorization.split(" ")[1];
-        
-        if(token){
-            decoded=jwt.verify(token,process.env.jwt_key);
-            console.log("cookies");
-        }
-        else if(token2){
-            decoded=jwt.verify(token2,process.env.jwt_key);
-        }
+        const token= req.headers.authorization.split(" ")[1];
+        const decoded=jwt.verify(token,process.env.jwt_key);
         req.body.userData=decoded;
         next();
     }catch(err){
-        console.log("it not there");
         return res.status(401).json({
             message:'Auth failed'
         })
