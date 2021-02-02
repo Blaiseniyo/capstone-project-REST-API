d177c329b2f549ac4486f9abe114fdc89d696d43
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2021-02-02 14:51:24 +0200

    adding a header in

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin,X-Requested-with,Content-Type,Accept,Authorization,header');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-with,Content-Type,Accept,Authorization,headers,Headers');
     if(req.method ==='OPTIONS'){
         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
         return res.status(200).json({});
